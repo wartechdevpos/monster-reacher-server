f451671dc410132f5a32923e6b6c1ff1e2a271cc
test <test>
2022-07-11 19:37:34 +0700

    fix twitter

--- gateway/api/oauth2/twitter.go
+++ gateway/api/oauth2/twitter.go
@@ -14,7 +14,7 @@ func getOAut2TwitterConfig() (*oauth2.Config, string, string, oauth2.AuthCodeOpt
 		ClientID:     "ckk3c1pxNVo5dGNnU0xhZ0Q1YWI6MTpjaQ",
 		ClientSecret: "vEuPsuxp7EJWrv8jCNI0gWRboA5hzk_WbyH5RiK01ZsV4CQMDJ",
 		RedirectURL:  "https://insuanhouse.ddns.net:3000/auth/twitter",
-		Scopes:       []string{"users.read", "tweet.read", "offline.access"},
+		Scopes:       []string{"users.read+tweet.read+offline.access"},
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  ENDPOINT_TWITTER_AUTH,
 			TokenURL: ENDPOINT_TWITTER_TOKEN,
